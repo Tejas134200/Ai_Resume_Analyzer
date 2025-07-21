f8411d5dc48b2daf7930d21bbc7625ab7d346d72
Tejas134200 <pokalwartejas30102005@gmail.com>
2025-07-21 21:22:41 +0530

    Resume feedback components

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -54,6 +54,7 @@ const upload=()=>{
         await kv.set(`resume:${uuid}`,JSON.stringify(data));
         setStatusText('Analysing Completed!');
         console.log(data)
+        navigate(`/resume/${uuid}`);
     }
     const handleSubmit = (e:FormEvent<HTMLFormElement>)=>{
         e.preventDefault();
